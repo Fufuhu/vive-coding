495430bd8f35f2ec04817b03840249acbe65cad7
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2025-04-04 22:36:41 +0900

    feat: enhance Login component with form elements and styling

--- vive-coding/src/component/login.tsx
+++ vive-coding/src/component/login.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-      {/* Add login form elements here */}
+    <div className="login-container">
+      <h2>Login Screen Mock</h2>
+      <label htmlFor="username">Username:</label>
+      <input type="text" id="username" name="username" />
+      <br />
+      <label htmlFor="password">Password:</label>
+      <input type="password" id="password" name="password" />
+      <br />
+      <button type="submit">Login</button>
     </div>
   );
 };
