eb827301395f6ab20b578903847ba46208d6d2fa
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2025-04-04 23:04:21 +0900

    feat: add password field icon to Login component for improved user experience

--- vive-coding/src/component/login.tsx
+++ vive-coding/src/component/login.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Box, Button, TextField } from '@mui/material';
 import { FaUser } from 'react-icons/fa';
+import { FaKey } from 'react-icons/fa'; // Added import statement
 
 const Login = () => {
   return (
@@ -14,12 +15,15 @@ const Login = () => {
           fullWidth
         />
       </Box>
-      <TextField
-        label="Password"
-        type="password"
-        margin="normal"
-        fullWidth
-      />
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <FaKey style={{ marginRight: '10px', fontSize: '150%' }} />
+        <TextField
+          label="Password"
+          type="password"
+          margin="normal"
+          fullWidth
+        />
+      </Box>
       <Button variant="contained" color="primary" fullWidth>
         Login
       </Button>
