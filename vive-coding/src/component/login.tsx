89b2fc97f57d0d50a5d36575f2f88d116b97efcd
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2025-04-04 23:00:24 +0900

    feat: enhance Login component with user icon in username field

--- vive-coding/src/component/login.tsx
+++ vive-coding/src/component/login.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Box, Button, TextField } from '@mui/material';
+import { FaUser } from 'react-icons/fa';
 
 const Login = () => {
   return (
     <Box display="flex" flexDirection="column" alignItems="center" justifyContent="flex-start"  bgcolor="grey.100" p={4} width="300px">
-      <TextField
-        label="Username"
-        type="text"
-        margin="normal"
-        fullWidth
-      />
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <FaUser style={{ marginRight: '10px', fontSize: '150%' }}/>
+        <TextField
+          label="Username"
+          type="text"
+          margin="normal"
+          fullWidth
+        />
+      </Box>
       <TextField
         label="Password"
         type="password"
