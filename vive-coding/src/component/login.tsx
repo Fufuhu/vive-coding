8840522a6756270adbbc7406ba98863d9bd2738a
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2025-04-04 22:34:13 +0900

    feat: add Login component with basic structure

---
+++ vive-coding/src/component/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      {/* Add login form elements here */}
+    </div>
+  );
+};
+
+export default Login;
